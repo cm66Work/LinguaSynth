f41cc715b3a24dd0faf07ceb376650d97d12c088
cm66Work <cm66.work@gmail.com>
2025-09-10 15:22:27 +0200

    Added: file upload to minio

--- Backend/LinguaSynth/app/src/main.py
+++ Backend/LinguaSynth/app/src/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI, UploadFile, File  # pyright: ignore[reportAssignmen
 from secrets import token_hex
 import os
 from MinIOManager import MinIOManager
+from PostgresManager import PostgresManager
 
+# --- Constants ---
+UPLOAD_BUCKET_NAME = 'upload'
 
+# --- Minio ---
 address = os.getenv('MINIO_ADDRESS', 'minio')
 port = os.getenv('MINIO_API_PORT', '9000')
 
@@ -17,13 +21,30 @@ if not username == 'minioadmin' or password == 'minioadmin':
     username = f.read()
   with open(password) as f:
     password = f.read()
-
 minioManager = MinIOManager(username, password, address, port)
 
-
+# --- API ---
 app = FastAPI()
 
+# --- Postgres ---
+address = os.getenv('POSTGRES_ADDRESS', 'db')
+port = os.getenv('POSTGRES_PORT', '5432')
+databaseName = os.getenv('POSTGRES_DB', 'test_db')
 
+# using the file so we need to read the contents
+username = os.getenv('POSTGRES_USER_FILE', 'test_user')
+password = os.getenv('POSTGRES_PASSWORD_FILE', 'test_password')
+# only update username and password if we had read a file path and not the default minio credentials
+# just in case we pass them instead of the actual files
+if not username == 'test_user' or password == 'test_password':
+  with open(username) as f:
+    username = f.read()
+  with open(password) as f:
+    password = f.read()
+postgresManager = PostgresManager(username, password, address, port, databaseName)
+
+
+# --- Code ---
 @app.get('/')
 async def root():
   return {'message:': 'Hello World!'}
@@ -35,13 +56,27 @@ async def HealthCheck():
 
 
 @app.post('/uploadfile/')
-async def UploadFile(bucketName: str, file: UploadFile = File(...)):  # pyright: ignore[reportGeneralTypeIssues]
-  fileExtension = file.filename.split('.').pop()  # eg: png, jpeg, txt, etc....
-  # encrypt the file name before upload.
-  fileName = token_hex(10)
-  filePath = f'{fileName}.{fileExtension}'
+async def UploadFile(file: UploadFile = File(...)):  # pyright: ignore[reportGeneralTypeIssues]
+  uploadResult = await HandleSummarizedFileGeneration(file)
+  # TODO:: Store the path to both the summarized file and the original file inside database
+  # TODO:: Pass the summarized file into Typesense
+  return {
+    'success': True,
+    'filePath': '',
+    'message': 'File uploaded successfully.',
+  }
+
+
+# --- Handlers ---
+async def HandleSummarizedFileGeneration(file: UploadFile = File(...)):  # type: ignore
+  # TODO:: Get or generate the schema
+  # TODO:: Create a temp file
+  filename = file.filename
+  filename = f'{filename.split(".")[0]}-summarized.{filename.split(".")[1]}'
   content = await file.read()
-  if not minioManager.BucketExists(bucketName):
-    minioManager.CreateBucket(bucketName)
-  minioManager.UploadFileContents(bucketName, filePath, content)
-  return {'success': True, 'filePath': filePath, 'message': 'File uploaded successfully.'}
+  if not minioManager.BucketExists(UPLOAD_BUCKET_NAME):
+    minioManager.CreateBucket(UPLOAD_BUCKET_NAME)
+  return minioManager.UploadFileContents(UPLOAD_BUCKET_NAME, filename, content)
+
+  # TODO:: Use LLM to summarize temp file using schema
+  # TODO:: upload the summarized file to Minio
