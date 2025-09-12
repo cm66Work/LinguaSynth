b05697658237f1727ca2980e0deb2bca5aef661e
cm66Work <cm66.work@gmail.com>
2025-09-12 13:25:45 +0200

    Init for LLMManager script

--- Backend/LinguaSynth/app/src/LLMManager.py
+++ Backend/LinguaSynth/app/src/LLMManager.py
@@ -1,3 +1,65 @@
+from ollama import Client, AsyncClient, GenerateResponse
+from tempfile import TemporaryFile
+from Utils.LogUtils import LogUtil
+from Utils.ServerResponse import (
+  ServerResponse,
+  ServerResponseObject,
+)
+
+
 class LLMManager:
-  def __init__(self) -> None:
+  def __init__(self, hostAddress: str, model='gemma3:1b'):
+    self.client = Client(host=hostAddress)
+    self.currentModel = model
+    self.serverResponseUtil = ServerResponse('Ollama', 'ollama_log')
+
+    # Logger
+    self.log = LogUtil('LLM', 'llm_log')
+
+  # --- Pulling Images ---
+  def PullImage(self, imageName: str):
+    result = self.client.pull(imageName)
+    return self.serverResponseUtil.GenerateServerResponse(
+      True, 'success', {'result': result}
+    )
+
+  # -- Switching models ---
+  def SwitchModel(self, imageName: str):
     pass
+
+  # --- Generating answers ---
+  def Generate(self, prompt=''):
+    pass
+
+  # --- Deleting models ---
+  def DeleteModel(self, modelName: str):
+    pass
+
+  # --- Get Models ---
+  def GetModels(self):
+    pass
+
+  # --- helper functions ---
+  def __GenerateResponse(
+    self, success: bool, message: str, extraData: dict = {}, generateLog=True
+  ):
+    """
+    Private helper function to keep return message code DRY.
+    Handles generating log messages for the action.
+
+    Args:
+      success (
+      bool): if the action was successful.
+      message (str): the message to log and return
+      extraData (dict): any extra information that should be returned
+    Return:
+      Object with both a result (bool) and message (str).
+      Also returns extraData on the end if any passed.
+    """
+    if not success:
+      if generateLog:
+        self.log.GenerateLogMessage(message)
+      return {'success': False, 'message': message, 'data': extraData}
+    if generateLog:
+      self.log.GenerateLogMessage(message)
+    return {'success': True, 'message': message, 'data': extraData}
---
+++ Backend/LinguaSynth/app/tests/LLM/test_LLMManager.py
@@ -0,0 +1,68 @@
+import os
+import pytest
+from LLMManager import LLMManager
+
+
+# --- Fixtures ---
+@pytest.fixture(scope='module')
+def Manager():
+  address = os.getenv('OLLAMA_ADDRESS', 'ollama')
+  port = os.getenv('OLLAMA_PORT', '11434')
+
+  return LLMManager(hostAddress=f'{address}:{port}', model='')
+
+
+# --- Pulling Images ---
+def test_pull_valid_model():
+  # result = Manager.PullImage('gemma3:1b')
+  # print(result)
+  # assert result.Success
+  pass
+
+
+def test_pull_invalid_model():
+  pass
+
+
+def test_pull_valid_model_new():
+  pass
+
+
+def test_pull_valid_model_duplicate_model():
+  pass
+
+
+# -- Switching models ---
+def test_switch_model_does_exist():
+  pass
+
+
+def test_switch_model_does_not_exist():
+  pass
+
+
+# --- Generating answers ---
+def test_generate_response_with_prompt():
+  pass
+
+
+def test_generate_response_with_empty_prompt():
+  pass
+
+
+# --- Deleting models ---
+def test_delete_model_does_exist():
+  pass
+
+
+def test_delete_model_does_not_exist():
+  pass
+
+
+# --- Get Models ---
+def test_get_models_exists():
+  pass
+
+
+def test_get_models_empty():
+  pass
