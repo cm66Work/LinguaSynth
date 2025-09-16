288deb1fdf48a2865d5aa92a259edd4080c0c90c
cm66Work <cm66.work@gmail.com>
2025-09-16 18:14:50 +0200

    moving to desktop

--- Backend/LinguaSynth/app/src/Managers/TypesenseManager.py
+++ Backend/LinguaSynth/app/src/Managers/TypesenseManager.py
@@ -1,2 +1,137 @@
 from Utils.ServerResponse import ServerResponse, ServerResponseObject
-import typesense
+import typesense  # type: ignore
+
+
+class TypesenseManager:
+  def __init__(
+    self,
+    host: str,
+    port: str,
+    protocol: str,
+    apiKey: str,
+    searchApiKey: str,
+  ):
+    self.client = typesense.Client(
+      {
+        'api_key': apiKey,
+        'nodes': [{'host': host, 'port': port, 'protocol': protocol}],
+        'connection_timeout_seconds': 5,
+      }
+    )
+    self.serverResponseUtil = ServerResponse('Typesense', 'typesense_log')
+
+  def RecreateCollection(self, schema: dict, schemaName: str) -> ServerResponseObject:
+    """
+    Deletes and recreates a new collection with the provides schema.
+    Args:
+        schema (dict): The schema used in the collection.
+    """
+    exceptionRaised = False
+    try:
+      self.client.collections[schemaName].delete()
+    except Exception as e:
+      self.serverResponseUtil.GenerateLogMessage(
+        f'Exception::TypesenseManager.CreateCollection:: {e}'
+      )
+      exceptionRaised = True
+      pass
+    result = self.client.collections.create(schema)
+    return self.serverResponseUtil.GenerateServerResponse(
+      success=exceptionRaised, message='', extraData={'result': result}
+    )
+
+  def DeleteCollection(self, name: str):
+    """
+    Deletes the collection if it exists.
+    Args:
+        name (str): Collection name to be deleted.
+    """
+    try:
+      result = self.client.collections[name].delete()
+      return self.serverResponseUtil.GenerateServerResponse(
+        success=True, message=f'Deleted collection {name}', extraData={'result': result}
+      )
+    except Exception as e:
+      return self.serverResponseUtil.GenerateServerResponse(
+        success=False, message=f'EXCEPTION::Typesense.DeleteCollection:: {e}'
+      )
+
+  def IndexDocuments(self, collection: str, documents: list[dict]):
+    """
+    Imports document content into Typesense
+    Args:
+        collection (str): Collection used to store the file.
+        documents (JSONLines): JSONLines list of documents to upload.
+          Format = [{id, schema files...},]
+    """
+    result = self.client.collections[collection].documents.import_(
+      documents, {'action': 'upsert'}
+    )
+
+    # Import method does not fail if a document fails to upload.
+    # So we need to sort our the failed documents from the success documents.
+    # Reasons for the failed upload are provided in the return object for each document.
+    errorList = []
+    for r in result:
+      if not r['success']:
+        errorList.append(r)
+    if len(errorList) > 0:
+      return self.serverResponseUtil.GenerateServerResponse(
+        success=False,
+        message=f'ERROR::TypesenseManager.IndexDocuments:: {len(errorList)} number of documents failed to import, see logs for more details',
+        extraData={'errors': errorList},
+      )
+    return self.serverResponseUtil.GenerateServerResponse(
+      success=True,
+      message=f'{len(result)} documents uploaded successfully.',
+      extraData={'result': result},
+    )
+
+  def NewQuery(
+    self, collection: str, question: str, queryBy='content,name', minHits=2, maxHits=20
+  ):
+    """
+    Queries typesense and applies rule response filters.
+    Args:
+        collection (str): Collection to query.
+        question (str): User query.
+        queryBy (): One or more field names that should be queried against (like adding filters to a search).
+        minHits (int): Number of hits required for the system to feel confident in the answer.
+        maxHits (int): Number of hits to trigger a "Question is not clear enough" response.
+    Returns:
+        Part of the return is extra data containing the following keys:
+          responseMessage (key:string): default answers to too few or too many documents being returned.
+          confidence (key: int): range from -1 to 1 based on how confident the system is about the response.
+          documents (key list[str]): Names of documents found.
+    """
+    search_params = {
+      'q': question,
+      'query_by': queryBy,
+    }
+    results = self.client.collections[collection].documents.search(search_params)
+    hits = results.get('hits', [])
+    n = len(hits)
+
+    responseMessage = ''
+    confidence = 0
+    documentNames = []
+    if n < minHits:
+      responseMessage = 'Not sure of the answer.'
+      confidence = -1
+    elif n > maxHits:
+      responseMessage = 'Question is not clear enough.'
+      confidence = 0
+    else:
+      documentNames = [h['document']['name'] for h in hits]
+      responseMessage = f'Found {n} documents.', documentNames
+      confidence = 1
+
+    return self.serverResponseUtil.GenerateServerResponse(
+      success=True,
+      message=f'Found {len(documentNames)} related to user query.',
+      extraData={
+        'responseMessage': responseMessage,
+        'confidence': confidence,
+        'documents': documentNames,
+      },
+    )
