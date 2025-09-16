40ce06725653f0d84a896732e0dbf398cb2adc66
Erica Marshall <569515@student.saxion.nl>
2025-09-16 23:46:06 +0200

    Added: pytests for typesense.

--- Backend/LinguaSynth/app/tests/Typesense/test_typesense.py
+++ Backend/LinguaSynth/app/tests/Typesense/test_typesense.py
@@ -3,12 +3,6 @@ from src.Managers.TypesenseManager import TypesenseManager
 import pytest
 
 # --- Constants ---
-TYPESENSE_HOST = os.getenv('TYPESENSE_HOST', 'typesense')
-TYPESENSE_PORT = os.getenv('TYPESENSE_PORT', '8108')
-TYPESENSE_PROTOCOL = os.getenv('TYPESENSE_PROTOCOL', 'http')
-TYPESENSE_SEARCH_API_KEY = os.getenv('TYPESENSE_SEARCH_KEY', 'xyz')
-TYPESENSE_API_KEY = os.getenv('TYPESENSE_API_KEY', 'xyz')
-
 TEST_COLLECTION = 'document_test'
 
 
@@ -21,7 +15,6 @@ def schema():
       {'name': 'name', 'type': 'string'},
       {'name': 'content', 'type': 'string'},
     ],
-    'default_sorting_field': 'id',
   }
 
 
@@ -30,41 +23,61 @@ def documents():
   return [
     {'id': '1', 'name': 'doc_alpha', 'content': 'The sky is blue and clear.'},
     {'id': '2', 'name': 'doc_beta', 'content': 'The ocean is vast and deep.'},
-    {'id': '3', 'name': 'doc_gamma', 'content': 'Mountains are tall and majestic.'},
+    {'id': '3', 'name': 'doc_beta1', 'content': 'The ocean is vast and deep.'},
+    {'id': '4', 'name': 'doc_beta2', 'content': 'The ocean is vast and deep.'},
+    {'id': '5', 'name': 'doc_beta3', 'content': 'The ocean is vast and deep.'},
+    {'id': '6', 'name': 'doc_beta4', 'content': 'The ocean is vast and deep.'},
+    {'id': '7', 'name': 'doc_beta5', 'content': 'The ocean is vast and deep.'},
+    {'id': '8', 'name': 'doc_beta6', 'content': 'The ocean is vast and deep.'},
+    {'id': '9', 'name': 'doc_gamma', 'content': 'Mountains are tall and majestic.'},
   ]
 
 
 @pytest.fixture(scope='module')
 def Manager(schema):
   return TypesenseManager(
-    host=TYPESENSE_HOST,
-    port=TYPESENSE_PORT,
-    protocol=TYPESENSE_PROTOCOL,
-    apiKey=TYPESENSE_API_KEY,
-    searchApiKey=TYPESENSE_SEARCH_API_KEY,
+    host=os.getenv('TYPESENSE_HOST', 'typesense'),
+    port=os.getenv('TYPESENSE_PORT', '8108'),
+    protocol=os.getenv('TYPESENSE_PROTOCOL', 'http'),
+    apiKey=os.getenv('TYPESENSE_API_KEY', 'xyz'),
+    searchApiKey=os.getenv('TYPESENSE_SEARCH_API_KEY', 'xyz'),
   )
 
 
 # --- Tests ---
-def test_create_collection(Manager):
-  result = Manager.RecreateCollection(schema, TEST_COLLECTION)
+def test_create_collection(Manager, schema):
+  result = Manager.RecreateCollection(schema, schemaName=schema['name'])
   assert result.Success
 
 
-def test_file_indexing_and_searching(Manager, documents):
+def test_file_indexing_and_searching(Manager, documents, schema):
   result = Manager.RecreateCollection(schema, TEST_COLLECTION)
   assert result.Success
-
-  response = Manager.IndexDocuments(TEST_COLLECTION, documents)
+  response = Manager.IndexDocuments(schema['name'], documents)
   assert response.Success  # will be true if all files are indexed correctly.
   response = Manager.NewQuery(TEST_COLLECTION, 'ocean')
   assert response.Success
-  assert len(response.Data['documents']) > 0
 
 
-# def test_search_with_few_result_case(manager, load_documents):
-#   pass
+def test_search_with_few_result_case(Manager, documents, schema):
+  result = Manager.RecreateCollection(schema, TEST_COLLECTION)
+  assert result.Success
+  response = Manager.IndexDocuments(schema['name'], documents)
+  assert response.Success  # will be true if all files are indexed correctly.
+  response = Manager.NewQuery(TEST_COLLECTION, 'sky')
+  assert response.Success
+  print(response)
+  assert response.Data['confidence'] == -1
+  assert len(response.Data['documents']) == 1  # documents with the word sky in it.
 
 
-# def test_search_with_many_results_case(manager, schema):
-#   pass
+def test_search_with_many_results_case(Manager, documents, schema):
+  result = Manager.RecreateCollection(schema, TEST_COLLECTION)
+  assert result.Success
+  response = Manager.IndexDocuments(schema['name'], documents)
+  assert response.Success  # will be true if all files are indexed correctly.
+  response = Manager.NewQuery(TEST_COLLECTION, 'ocean', maxHits=3)
+  assert response.Success
+  print(response)
+  assert response.Data['confidence'] == 0
+  assert len(response.Data['documents']) == 7  # documents with the word ocean in it.
